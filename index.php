283c148e4e17b8fdab819cf77295e53e03983f76
Michelle <mbojorquez4@cnm.edu>
2017-09-04 20:10:31 -0600

    carasol works no photos:(

--- index.php
+++ index.php
@@ -1,14 +1,77 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>pwp-project</title>
+    <!-- Theme Made By www.w3schools.com - No Copyright -->
+    <title>Bootstrap Theme The Band</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+        .container {
+            padding: 80px 120px;
+        }
+        .person {
+            border: 10px solid transparent;
+            margin-bottom: 25px;
+            width: 80%;
+            height: 80%;
+            opacity: 0.7;
+        }
+        .person:hover {
+            border-color: #f1f1f1;
+        }
+         div.gallery {
+             border: 1px solid #ccc;
+         }
+
+        div.gallery:hover {
+            border: 1px solid #777;
+        }
+
+        div.gallery img {
+            width: 100%;
+            height: auto;
+        }
+
+        div.desc {
+            padding: 15px;
+            text-align: center;
+        }
+
+        * {
+            box-sizing: border-box;
+        }
+
+        .responsive {
+            padding: 0 6px;
+            float: left;
+            width: 24.99999%;
+        }
+
+        @media only screen and (max-width: 700px){
+            .responsive {
+                width: 49.99999%;
+                margin: 6px 0;
+            }
+        }
+
+        @media only screen and (max-width: 500px){
+            .responsive {
+                width: 100%;
+            }
+        }
+
+        .clearfix:after {
+            content: "";
+            display: table;
+            clear: both;
+        }
+    </style>
 </head>
 <body>
+
 
 <nav class="navbar navbar-inverse">
     <div class="container-fluid">
@@ -24,27 +87,51 @@
     </div>
 </nav>
 <div class="container text-center">
-    <h3>D&M Development</h3>
+    <h2>D&M Development</h2>
     <p><em>We love our clients</em></p>
     <p>We want to help you create the best website ever.</p>
-    <br>
-    <div class="row">
-        <div class="col-sm-4">
-            <p><strong>Inspire</strong></p><br>
-            <img src="" alt="Inspire">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src="" alt="Explore">
+                <div class="carousel-caption">
+                    <h3>Explore</h3>
+                    <p>Explore beyond your horizons.</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="img/pexels-photo-54204.jpeg" alt="Inspire">
+                <div class="carousel-caption">
+                    <h3>Inspire</h3>
+                    <p>Inspire the people around you.</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="img/" alt="Discover">
+                <div class="carousel-caption">
+                    <h3>Discover</h3>
+                    <p>Discover your best.</p>
+                </div>
+            </div>
         </div>
-        <div class="col-sm-4">
-            <p><strong>Explore</strong></p><br>
-            <img src="" alt="Explore">
-        </div>
-        <div class="col-sm-4">
-            <p><strong>intoxicate</strong></p><br>
-            <img src="/img/above-adventure-aerial-air.jpg" alt="intoxicate">
-        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
-</div>
-
-
-
-</body>
-</html>
